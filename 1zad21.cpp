9a258d265dde02f397fd15f8cb74a2e64859cc22
mariaantipina <91631167+mariaantipina@users.noreply.github.com>
2021-10-26 11:38:15 +0300

    Update 1zad21.cpp

--- 1zad21.cpp
+++ 1zad21.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 
 int n;
-int razdelno(int rez) {
+void razdelno(int rez) {
 	if (n / rez)
 		razdelno(rez * 10);
 	rez /= 10;
 	cout << n / rez << endl;
 	n %= rez;
-	return 0;
 }
 
 void main() {
